7048c8a148da31a9d68739484e387ce4625310bb
khanhvy1703 <kvy.le@protonmail.com>
2025-09-17 15:36:50 -0400

    add Name

--- src/app/Labs/Lab1/page.tsx
+++ src/app/Labs/Lab1/page.tsx
@@ -6,6 +6,7 @@ export default function Lab1() {
   return (
     <div id="wd-lab1">
       <h2>Lab 1</h2>
+      <h3>Full name: Vy Le</h3> 
       <h3>HTML Examples</h3>
       <div id="wd-h-tag">
         <h4>Heading Tags</h4>
