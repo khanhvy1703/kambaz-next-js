064c88096346ed93cee7b17aff827e0a6ff39637
khanhvy1703 <kvy.le@protonmail.com>
2025-10-01 20:30:45 -0400

    error

--- src/app/(Kambaz)/Account/Signin/page.tsx
+++ src/app/(Kambaz)/Account/Signin/page.tsx
@@ -15,7 +15,7 @@ export default function Signin() {
       
       <Link
         id="wd-signin-btn"
-        href="/Account/Profile"
+        href="/Dashboard"
         className="btn btn-primary w-100 mb-2"
       >
         Sign in{" "}
