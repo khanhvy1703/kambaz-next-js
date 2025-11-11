868bd1bb2590e28bb08e8e1c1030144ca614bf32
khanhvy1703 <kvy.le@protonmail.com>
2025-11-11 15:36:07 -0500

    till 5.2.4

--- src/app/Labs/page.tsx
+++ src/app/Labs/page.tsx
@@ -25,7 +25,12 @@ export default function Labs() {
           </Link>
         </li>
         <li>
-          <Link href="/" id="wd-lab3-link">
+          <Link href="/Labs/Lab5" id="wd-lab5-link">
+            Lab 5{" "}
+          </Link>
+        </li>
+        <li>
+          <Link href="/" id="wd-kambaz-link">
           Kambaz </Link> 
         </li>
       </ul>
